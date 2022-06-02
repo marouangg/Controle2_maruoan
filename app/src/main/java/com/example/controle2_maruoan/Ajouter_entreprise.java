65360a50bf87092bca3bba3e67da507f03f9810d
marouangg <marouaonda66@gmail.com>
2022-06-02 15:32:30 +0100

    exo7__exo8___exo9

--- app/src/main/java/com/example/controle2_maruoan/Ajouter_entreprise.java
+++ app/src/main/java/com/example/controle2_maruoan/Ajouter_entreprise.java
@@ -3,6 +3,7 @@ package com.example.controle2_maruoan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Ajouter_entreprise extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Ajouter_entreprise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_entreprise);
+    }
+
+    public void ajouterProduit(View view) {
     }
 }
--- app/src/main/java/com/example/controle2_maruoan/MainActivity.java
+++ app/src/main/java/com/example/controle2_maruoan/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.controle2_maruoan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void acces(View view) {
+
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.b1: i=new Intent(MainActivity.this, Ajouter_entreprise.class); break;
+            case R.id.b2: i=new Intent(MainActivity.this, editer_entreprise.class); break;
+            case R.id.b3: i=new Intent(MainActivity.this, Liste_entreprise.class); break;
+
+        }
+        startActivity(i);
     }
 }
