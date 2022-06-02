ce78e485df77f9fe11bcffc01c49210d0be699de
marouangg <marouaonda66@gmail.com>
2022-06-02 15:00:31 +0100

    exo7

---
+++ app/src/main/java/com/example/controle2_maruoan/Ajouter_entreprise.java
@@ -0,0 +1,14 @@
+package com.example.controle2_maruoan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ajouter_entreprise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_entreprise);
+    }
+}
